64775504ddbf48410b6cc517a77df0946cb8cd86
kodmit <alexandre.bailly>
2019-05-20 20:43:39 +0200

    wip

---
+++ pages/group/_id.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="view_group">
+    <section class="main-content columns is-fullheight">
+      <Sidebar />
+      <div class="column is-10 manage_group">
+        Content
+      </div>
+    </section>
+    <Footer />
+  </div>
+</template>
+<script>
+import Footer from '../../components/Footer'
+import Sidebar from "../../components/dashboard/Sidebar";
+export default {
+  name: 'ViewGroup',
+  components: {Sidebar, Footer },
+  head() {
+    return {
+      title: 'Groupe'
+    }
+  },
+  data: function () {
+    return {
+      group: ''
+    }
+  },
+  mounted() {
+    const id = this.$route.params.id
+    const self = this
+    this.$axios.get('/group/' + id)
+      .then(function (response) {
+        if (response.status === 200) {
+          console.log('ok')
+        }
+      })
+      .catch(function (error) {
+        if (error.response) {
+          if (error.response.status === 404) {
+            self.$router.push('/group/not_found')
+          }
+        }
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/group/not_found.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not_found">
+    <i class="fal fa-meh fa-5x _icon" />
+    <span class="title">Groupe introuvable</span>
+    <nuxt-link to="../dashboard">
+      Retour
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupNotFound',
+  head() {
+    return {
+      title: 'Groupe introuvable'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/css/dashboard.css';
+</style>
